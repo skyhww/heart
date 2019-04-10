2f291b3f00919dda36132c381efa3cf292366037
hanweiwei <weiwei.han@zhangmen.com>
2019-04-10 15:57:36 +0800

    文件上传下载

--- controller/iMessage.go
+++ controller/iMessage.go
@@ -1,9 +1,7 @@
 package controller
 
 import (
-	"github.com/gorilla/websocket"
 	"github.com/astaxie/beego"
-	"fmt"
 )
 
 type IMessageController struct {
@@ -11,11 +9,11 @@ type IMessageController struct {
 } 
 
 func (iMessageController *IMessageController) Get(){
-	conn, err := websocket.Upgrader{}.Upgrade(iMessageController.Ctx.ResponseWriter, iMessageController.Ctx.Request, nil)
+	/*conn, err := websocket.Upgrader{}.Upgrade(iMessageController.Ctx.ResponseWriter, iMessageController.Ctx.Request, nil)
 	if err != nil {
 		iMessageController.Ctx.ResponseWriter.Status=406
 		iMessageController.Ctx.ResponseWriter.Flush()
 		return
 	}
-	fmt.Print(conn)
+	fmt.Print(conn)*/
 }
